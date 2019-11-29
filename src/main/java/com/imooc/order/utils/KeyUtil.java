c6f73a9ba14c1cc4b9b6eda7f742ab847343325f
马锐敏 <351489917@qq.com>
2019-11-29 17:46:00 +0800

    创建订单接口

---
+++ src/main/java/com/imooc/order/utils/KeyUtil.java
@@ -0,0 +1,15 @@
+package com.imooc.order.utils;
+
+import java.util.Random;
+
+public class KeyUtil {
+    /**
+     * 生成唯一的主键
+     * 格式：时间+随机数
+     */
+    public static synchronized String genUniqueKey() {  // synchronized表示在多线程的情况下，避免生成相同的key
+        Random random = new Random();
+        Integer number = random.nextInt(900000) + 100000;  //生成6位的随机数
+        return System.currentTimeMillis() + String.valueOf(number);
+    }
+}
